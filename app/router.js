506eaba2406ff3f935cf8bbe5c419f04bdcfe6ef
huahaitao <huahaitao739@pingan.com.cn>
2018-06-26 23:39:25 +0800

    添加静态资源

--- app/router.js
+++ app/router.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
-  router.get('/news', controller.news.list);
+  require('./router/api')(app);
+  require('./router/web')(app);
 };
